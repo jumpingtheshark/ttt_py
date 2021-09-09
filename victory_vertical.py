921fb4359ba378cee94a0c1d60f97687f5d727d6
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-09-09 14:46:00 -0500

    Add files via upload

--- victory_vertical.py
+++ victory_vertical.py
@@ -1,4 +1,4 @@
-board = [["*","*","*"],["#","#","#"],["$","$","$"]]
+board = [["1","#","13"],["21","#","23"],["*","#","$"]]
 
 
 game_over=False
@@ -12,8 +12,9 @@ def all_same (cells, symbol):
 
 #check columns
 l=[]
-for i in range(0,3):
-    l.append(board[i][0])
-
-print (l)
-print(all_same(l, board[i][0]))
+for i in range (0,3):
+    l=[]
+    for j in range(0,3):
+        l.append(board[i][j])
+    print (l)
+    print(all_same(l, board[i][0]))
