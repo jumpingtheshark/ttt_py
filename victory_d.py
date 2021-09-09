bd64bed02314fd9dda91486405a390cef0aceb09
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-09-09 14:33:21 -0500

    Update victory_d.py

--- victory_d.py
+++ victory_d.py
@@ -1,1 +1,12 @@
+board = [["*","*","*"],["#","#","#"],["$","$","$"]]
+
+print ("victory_diagonal.py")
+game_over=False
+
+def all_same (cells, symbol):
+    rval = True
+    for i in range (0,3):
+        if cells[i] != symbol:
+            rval = False
+    return rval
 
